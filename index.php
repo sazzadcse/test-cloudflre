34fb280d3b7894adfe010afd85ef7f1419e3fd95
sazzadcse <sazzadtbd@gmail.com>
2023-02-08 14:13:37 +0600

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo 'Hello Happy Monster with Cloudflare Page!!'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Happy Monster - Cloudflare</title>
+</head>
+<body>
+    <div class="main">
+        <h2 class="pagetitle">Welcome to HappyMonster with Cloudflare!!</h2>
+    </div>
+</body>
+</html>
